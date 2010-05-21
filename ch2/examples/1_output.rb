abd1276d2966a20cab686fefc8204b553d826e48
Josh Cheek <josh.cheek@gmail.com>
2010-05-21 14:14:05 -0500

    added output to ch2

---
+++ ch2/examples/1_output.rb
@@ -0,0 +1,11 @@
+puts "first line"
+
+print "this will all be "
+print "on the same line"
+
+puts # an empty newline
+
+# field types can be seen at http://ruby-doc.org/core/classes/Kernel.html#M005962
+printf "%d is %b in binary, %x in hex, and %o in octal\n" , 95 , 95 , 95 , 95
+printf "%10s\n" , 'hello'
+printf "%10s\n" , 'world'
---
+++ ch2/notes/josh.rb
@@ -0,0 +1,13 @@
+# josh's notes for presenting ch2
+
+# =====  Stdout  =====
+# Java                  Ruby
+# System.out.println    puts
+# System.out.print      print
+# System.out.printf     printf
+#
+# all IO have these methods, not just stdout
+# show example 1
+
+
+# ====
