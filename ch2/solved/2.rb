bc2b4b9c86c96f026fc67280fa88fc7a3f332e54
Josh Cheek <josh.cheek@gmail.com>
2010-05-27 11:35:04 -0500

    finished tests for 2:3 and 2:4

--- ch2/solved/2.rb
+++ ch2/solved/2.rb
@@ -1,41 +1,15 @@
-# This challenge is an extension of example 2, so we've included its source as a starting point
-#
-# Prompt the user for a number, then read it in and print out "hi" that many times
-# 
-# Repeat this process until the user submits "bye", then say "goodbye" and end the program
-#
-#         example:
-#         
-#         PROGRAM: Enter a number or bye
-#         USER:    4
-#         PROGRAM: hi hi hi hi
-#         PROGRAM: Enter a number or bye
-#         USER:    2
-#         PROGRAM: hi hi
-#         PROGRAM: Enter a number or bye
-#         USER:    bye
-#         PROGRAM: goodbye
-
-
-# remember you can try your program out with              $ ruby 2_input_output_control.rb
-# and when you think it is correct, you can test it with  $ rake 2:2
-
 def prompt
   puts 'Enter a number or bye'
 end
 
 def hi_hi_goodbye
-  
   prompt
-
   while ( line = gets ) && ( line !~ /bye/ )
     line.to_i.times { print 'hi ' }
     puts
     prompt
   end
-
   puts "Goodbye!"
-  
 end
 
 
---
+++ ch2/solved/3.rb
@@ -0,0 +1,13 @@
+# Given an array of elements
+# return a new array length 2 containing the first and last elements from the original array.
+# The original array will be length 1 or more.
+# 
+# make_ends( [1, 2, 3]    )                     # =>  [1, 3]
+# make_ends( [1, 2, 3, 4] )                     # =>  [1, 4]
+# make_ends( [7, 4, 6, 2] )                     # =>  [7, 2]
+# make_ends( %w[the quick brown fox] )          # =>  ['the', 'fox']
+# make_ends( [/r/ , /e/ , /g/ , /e/ , /x/] )    # =>  [/r/, /x/]
+
+def make_ends(ary)
+  [ ary.first , ary.last ]
+end
