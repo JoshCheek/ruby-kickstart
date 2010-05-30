66d0b581c8e779817d0ebc8db940ea4f26b8dc8b
Josh Cheek <josh.cheek@gmail.com>
2010-05-30 18:24:28 -0500

    made spec for 2:1 more versatile

--- ch2/examples/1_output.rb
+++ ch2/examples/1_output.rb
@@ -1,7 +1,7 @@
 number = 5
 other_number = 12
 
-puts "first line"
+puts "first line" , "second line"
 
 puts "The number we have is #{number} and the sum of our numbers is #{number + other_number}" 
 # >> The number we have is 5 and the sum of our numbers is 17
