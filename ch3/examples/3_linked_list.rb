d3c92e4597a9b7d2c16bafa004ce1881627eff69
Josh Cheek <josh.cheek@gmail.com>
2010-06-03 16:25:31 -0500

    added linked list example

---
+++ ch3/examples/3_linked_list.rb
@@ -0,0 +1,61 @@
+class LinkedList
+  
+  class Node
+    attr_accessor :next , :data
+    def initialize( options = Hash.new )
+      self.next , self.data = options[:next] , options[:data]
+    end
+  end
+
+  include Enumerable
+  
+  def initialize(*params)
+    self.first_node = nil
+    params.each { |param| self << param }
+  end
+
+  def each(&block)
+    each_node { |node| block.call node.data }
+    self
+  end
+  
+  def <<(data)
+    new_node = Node.new :next => first_node , :data => data
+    self.first_node = new_node
+    self
+  end
+  
+  def delete( data_to_delete = nil , &block )
+    raise ArgumentError.new("must pass either data or a select block") unless data_to_delete || block
+    return Array.new unless first_node
+    block ||= lambda { |data| data == data_to_delete }
+    each_node do |node|
+      next_node = node.next
+      node.next = next_node.next if next_node && block[next_node.data]
+    end
+    self.first_node = first_node.next if block[first_node]
+  end
+  
+private
+
+  attr_accessor :first_node
+  
+  def each_node
+    node = first_node
+    while node
+      yield node
+      node = node.next
+    end
+  end
+  
+end
+
+
+ll = LinkedList.new 77,90,"jkl",[67,90],{0=>8,:acv=>98}
+ll << 5 << 6 << 9 << 10 << 'abc'
+
+ll.each { |data| p data }
+p ll.select { |data| data.is_a?(Numeric) && data % 2 == 0 }
+puts
+ll.delete { |data| data == 5 || data == 10 }
+ll.each { |data| p data }
