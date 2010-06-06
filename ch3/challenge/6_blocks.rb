b440f490f57d2017134be5b1cca318ac524f2545
Kevin Griffin <kevin@tmbox.com>
2010-06-06 17:42:48 -0500

    Working on 6

--- ch3/challenge/6_blocks.rb
+++ ch3/challenge/6_blocks.rb
@@ -22,37 +22,36 @@
 # NOTE: This code will only work with the rake tests, which will define the order and current_user
 # you will not be able to run this code outside of the test
 
-
-def pay_by_visa(order,ccn)
+def pay_by(order)
   order.compute_cost
   order.compute_shipping
   order.compute_tax
+  yield
+  order.ship_goods
+end
+  
+def pay_by_visa(order,ccn)
+  pay_by(order) do 
   order.payment :type => :visa , :ccn => ccn
   order.verify_payment
-  order.ship_goods
+  end
 end
 
 def pay_by_check(order)
-  order.compute_cost
-  order.compute_shipping
-  order.compute_tax
+  pay_by(order) do 
   order.payment :type => :check , :signed => true
-  order.ship_goods
+  end
 end
 
 def pay_by_cash(order)
-  order.compute_cost
-  order.compute_shipping
-  order.compute_tax
+  pay_by(order) do
   order.payment :type => :cash
-  order.ship_goods
+  end
 end
 
-def pay_by_store_credit(order)
-  order.compute_cost
-  order.compute_shipping
-  order.compute_tax
+def pay_by_store_credit(order, current_user)
+  pay_by(order) do
   order.payment :type => :store_credit
   current_user.store_credit -= order.cost   # current_user is a method with no params (ie, the customer)
-  order.ship_goods
+  end
 end
