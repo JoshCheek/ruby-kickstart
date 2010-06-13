21ecec6398dcfabdb4cbdc21e51fd599aa4f64e9
Josh Cheek <josh.cheek@gmail.com>
2010-06-13 09:51:17 -0500

    finished ch6

---
+++ ch4/challenge/6_string.rb
@@ -0,0 +1,18 @@
+# We'll say that a "triple" in a string is a char appearing three times in a row. Return the number of triples in the given string. The triples may overlap. 
+# 
+# count_triple("abcXXXabc")       # => 1
+# count_triple("xxxabyyyycd")     # => 3
+# count_triple("a")               # => 0
+# count_triple("")                # => 0
+# count_triple("XXXabc")          # => 1
+# count_triple("XXXXabc")         # => 2
+# count_triple("XXXXXabc")        # => 3
+# count_triple("222abyyycdXXX")   # => 3
+# count_triple("abYYYabXXXXXab")  # => 4
+# count_triple("abYYXabXXYXXab")  # => 0
+# count_triple("abYYXabXXYXXab")  # => 0
+# count_triple("122abhhh2")       # => 1
+
+
+def count_triple
+end
