30d36272fe7c796846a788de2e2ccf6d31f5a674
Kevin Griffin <kevin@tmbox.com>
2010-06-17 14:51:14 -0500

    Clarification in instructions.

--- ch4/challenge/7_array.rb
+++ ch4/challenge/7_array.rb
@@ -1,4 +1,4 @@
-# Given an array of ints, return true if the number of 1's is greater than the number of 4's 
+# Given an arbitrary number of ints, return true if the number of 1's is greater than the number of 4's 
 #
 # more14(1, 4, 1)             # =>  true
 # more14(1, 4, 1, 4)          # =>  false
