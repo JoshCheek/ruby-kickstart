3bf1fb6132c8444a1a2b6d9281291633dd7fc008
Josh Cheek <josh.cheek@gmail.com>
2011-05-19 18:42:01 -0500

    explain stdin and stdout

--- ch1/notes/11-puts.rb
+++ ch1/notes/11-puts.rb
@@ -1,4 +1,11 @@
-# Ruby has several methods for output, the most common of which is 'puts':
+# Programs that run in a terminal read text in, and write text out. 
+# This is the common interface that allows lots of different programs to interact with each other.
+
+# When a program outputs text, we say it sends it to standard output.
+# In the terminal, anything sent to standard output will display on your terminal.
+# For example, cat sends a file's contents to standard output, echo sends its arguments to standard output.
+
+# Ruby has several methods for outputting text, the most common of which is 'puts':
 puts "Welcome to the program!"
 
   # Output:
