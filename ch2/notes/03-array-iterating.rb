cf779d4a227d51d99de23fa9943512f205089502
Josh Cheek <josh.cheek@gmail.com>
2011-05-28 05:14:56 -0500

    add each_cons and each_slice

--- ch2/notes/03-array-iterating.rb
+++ ch2/notes/03-array-iterating.rb
@@ -12,3 +12,16 @@ sum # => 45
 squares = numbers.map { |number| number * number }
 squares # => [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 numbers # => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+# iterate over the array two at a time
+result = []
+numbers.each_slice(2) { |a, b| result << [a, b] }
+result # => [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]]
+
+
+# iterate over the array in groups of two
+# hitting each group as we go
+result = []
+numbers[0..5].each_cons(2) { |a, b| result << [a, b] }
+result # => [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5]]
