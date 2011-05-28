3c2699d559998fbfc9a656a3f84c771836832727
Josh Cheek <josh.cheek@gmail.com>
2011-05-28 05:08:26 -0500

    note where cheatsheet is, mention explicit return

---
+++ ch2/notes/19-last-words.rb
@@ -0,0 +1,16 @@
+# Okay, ready for challenges ^_^
+# I spent a long time on these, hopefully you'll enjoy them!
+
+
+
+# A USEFUL HINT!
+# You can return from a method early with the keyword return
+def method(condition)
+  return 1 if condition
+  2
+end
+method true  # => 1
+method false # => 2
+
+# Check out cheatsheet for arrays:
+# https://github.com/JoshCheek/ruby-kickstart/blob/master/cheatsheets/arrays.rb
