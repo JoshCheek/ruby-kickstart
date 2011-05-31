7b88be9456af51726189878aeed15d8ff39dd2d3
Josh Cheek <josh.cheek@gmail.com>
2011-05-31 14:12:15 -0500

    use map now, since we covered it in the session

--- ch2/solved/1.rb
+++ ch2/solved/1.rb
@@ -1,10 +1,5 @@
 def sum_difference_product
-  # wouldn't it be great if we had a method that could perform an action
-  # on each element of the array so that we didn't have to say n = n.to_i for all the elements in the array?
-  # keep that in mind for the future ;)
-  a , b = gets.split
-  a = a.to_i
-  b = b.to_i
+  a , b = gets.split.map { |num| num.to_i }
   puts a + b
   puts a - b
   puts a * b
