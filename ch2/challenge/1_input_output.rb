b01a765116305aec807c8a42b7023271e385c22f
Josh Cheek <josh.cheek@gmail.com>
2011-05-31 14:12:52 -0500

    make instructions a little clearer

--- ch2/challenge/1_input_output.rb
+++ ch2/challenge/1_input_output.rb
@@ -1,8 +1,11 @@
-# remember you can test this code with
+# Remember you can test this code with
 #   $ rake 2:1
 
-# write a program that reads in two integers typed on the keybaord
+# Write a program that reads in two integers typed on the keybaord
 # and outputs their sum, difference, and product
+# 
+# Standard input will be like "9 2\n" and will expect you to print
+# "11\n7\n18\n" to standard output.
 
 def sum_difference_product
   # your code goes here
