8c79fddb102b49d0f011dc2cff4972dcd55c5478
Josh Cheek <josh.cheek@gmail.com>
2011-06-04 01:44:41 -0500

    add a new challenge 1

---
+++ ch3/challenge/1_blocks.rb
@@ -0,0 +1,8 @@
+# Write a method, reverse_map, which invokes a block on each of the elements in reverse order,
+# and returns an array of their results.
+# 
+# reverse_map( 1, 2, 3 ) { |i| i * 2 }      # => [6, 4, 2]
+
+def reverse_map(*args, &block)
+  args.reverse.map(&block)
+end
--- ch3/spec/1.rb
+++ ch3/spec/1.rb
@@ -1,111 +1,27 @@
-describe 'HTMLTag' do
+describe 'reverse_map' do
   
-  it "should generate \"<li style='color:#FF0000;'>baseball</li>\\n\" when given 'li' , 'baseball' , :multiline => false , :color => :red" do
-    HTMLTag.new( 'li' , 'baseball' , :multiline => false , :color => :red ).to_s.chomp.should =~ %r{<li\s+style=('|")color:#FF0000;(\1)\s*>baseball</li>}i
-  end
-  
-  it "should generate \"<li style='color:#00FF00;'>baseball</li>\\n\" when given 'li' , 'baseball' , :multiline => false , :color => :green" do
-    HTMLTag.new( 'li' , 'baseball' , :multiline => false , :color => :green ).to_s.chomp.should =~ %r{<li\s+style=('|")color:#00FF00;(\1)\s*>baseball</li>}i
+  it 'should have defined a reverse_map method' do
+    method(:reverse_map).should be
   end
   
-  it "should generate \"<li style='color:#0000FF;'>baseball</li>\\n\" when given 'li' , 'baseball' , :color => :blue" do
-    HTMLTag.new( 'li' , 'baseball' , :color => :blue ).to_s.chomp.should =~ %r{<li\s+style=('|")color:#0000FF;(\1)\s*>baseball</li>}i
+  specify '( 1, 2, 3 ) { |i| i * 2 } # => [6, 4, 2]' do
+    reverse_map( 1, 2, 3 ) { |i| i * 2 }.should == [6, 4, 2]
   end
   
-  it "should generate \"<p >soccer</p>\\n\" when given 'p' , 'soccer' , :multiline => false" do
-    HTMLTag.new( 'p' , 'soccer' , :multiline => false ).to_s.chomp.should =~ %r{<p\s*>soccer</p>}i
-  end
-
-  it "should generate \"<p >soccer</p>\\n\" when given 'p' , 'soccer' , Hash.new" do
-    HTMLTag.new( 'p' , 'soccer' , Hash.new ).to_s.chomp.should =~ %r{<p\s*>soccer</p>}i
-  end
-
-  it "should generate \"<p >soccer</p>\\n\" when given 'p' , 'soccer' , and no hash" do
-    HTMLTag.new( 'p' , 'soccer' ).to_s.chomp.should =~ %r{<p\s*>soccer</p>}i
+  specify '{} # => []' do
+    reverse_map {}.should == []
   end
   
-  it "should generate \"<li style='font-family:\"Arial\", \"Verdana\";color:#FF0000;'>baseball</li>\\n\" when given 'li' , 'baseball' , :multiline => false , :color => :red , :font => :sans_serif" do
-    regex = %r{<li\s+style=('|")(.*)(\1)\s*>baseball</li>}i
-    html  = HTMLTag.new( 'li' , 'baseball' , :multiline => false , :color => :red , :font => :sans_serif ).to_s.chomp
-    html.should =~ regex
-    html =~ regex   # because capture groups aren't _really_ global, and rspec changes scope of execution
-    style = $2
-    style.should =~ /font-family:("|')Arial(\1), ("|')Verdana(\3);color:#FF0000;/i
+  specify '# => []' do
+    reverse_map.to_a.should == []
   end
   
-  it 'multiline switch should work' do
-    HTMLTag.new( 'p' , 'soccer' , :multiline => true ).to_s.chomp.should =~ %r{<p\s*>\nsoccer\n</p>}i
-  end
-
-end
-
-
-
-
-
-describe 'The example from the explanation' do
-    
-  before :each do
-    sports = [
-      HTMLTag.new( 'li' , 'baseball' , :multiline => false , :color => :red   , :font => :serif      ) ,
-      HTMLTag.new( 'li' , 'soccer'   , :multiline => false , :color => :green , :font => :sans_serif ) ,
-      HTMLTag.new( 'li' , 'football' , :multiline => false , :color => :blue  , :font => :monospace  ) ,
-    ]
-  
-    ordered_list = HTMLTag.new 'ol' , sports.join , :multiline => true
-  
-    @lines = ordered_list.to_s.split("\n")
-
-    @regexes = {
-      :open     => /<ol\s*>/i ,  
-      :baseball => { :tag => %r{<li\s+style=('|")(.*?)(\1)>baseball</li>}i , :styles => [/color\s*:\s*#FF0000\s*;/i , /font-family:("?|'?)Times New Roman(\1),\s*("?|'?)Georgia(\3);/     ] },
-      :soccer   => { :tag => %r{<li\s+style=('|")(.*?)(\1)>soccer</li>}i   , :styles => [/color\s*:\s*#00FF00\s*;/i , /font-family:("?|'?)Arial(\1),\s*("?|'?)Verdana(\3);/               ] },
-      :football => { :tag => %r{<li\s+style=('|")(.*?)(\1)>football</li>}i , :styles => [/color\s*:\s*#0000FF\s*;/i , /font-family:("?|'?)Courier New(\1),\s*("?|'?)Lucida Console(\3);/  ] },
-      :close    => %r(</ol>)i,
-    }
-  end
-    
-  it 'should result in five lines' do
-    @lines.size.should == 5
+  specify '("a", "b", "c") { |char| char * 3 } # => ["ccc", "bbb", "aaa"]' do
+    reverse_map("a", "b", "c") { |char| char * 3 }.should == ["ccc", "bbb", "aaa"]
   end
   
-  its 'first tag should be ordered list' do
-    @lines[0].should =~ @regexes[:open]
-  end
-
-  its 'second line should be the li for baseball' do
-    @lines[1].should  =~  @regexes[:baseball][:tag]
+  specify '(*1..1000) { |i| i * i } # => [1000000, 998001, ..., 9, 4, 1]' do
+    reverse_map(*1..1000) { |i| i * i }.should == (1..1000).to_a.reverse.map { |i| i * i }
   end
   
-  its "second line's style should match the expected styles" do
-    @lines[1] =~ @regexes[:baseball][:tag]
-    style = $2
-    @regexes[:baseball][:styles].each { |regex| style.should =~ regex }
-  end
-  
-  
-  its 'third line should be the li for soccer' do  
-    @lines[2].should  =~  @regexes[:soccer][:tag]
-  end
-  
-  its 'third line should match the expected styles' do
-    @lines[2] =~ @regexes[:soccer][:tag]
-    style = $2
-    @regexes[:soccer][:styles].each { |regex| style.should =~ regex }
-  end
-  
-  its 'fourth line should be the li for football' do
-    @lines[3].should  =~  @regexes[:football][:tag]    
-  end
-  
-  its 'fourth lines style should match the expected styles' do
-    @lines[3] =~ @regexes[:football][:tag]
-    style = $2
-    @regexes[:football][:styles].each { |regex| style.should =~ regex }
-  end
-    
-  its 'fifth line should close the ordered list' do
-    @lines[4].should  =~  @regexes[:close]
-  end  
-
 end
