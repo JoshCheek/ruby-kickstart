3366362a37abc5d2afa1caa49a4f61027cab77b6
Josh Cheek <josh.cheek@gmail.com>
2011-06-04 00:23:49 -0500

    update to be simpler and consistent with 1.9.2

--- cheatsheets/hashes.rb
+++ cheatsheets/hashes.rb
@@ -18,26 +18,12 @@ hash[:josh]       # => nil
 
 # to change this behaviour, you can pass new a block that receives the hash and the nonexistent key
 # and then adds it to the hash in the way that you would like
-aliases = Hash.new { |hash,key| hash[key] = Array.new }
+aliases = Hash.new { |this_hash, key| this_hash[key] = Array.new }
 aliases         # => {}
 aliases[:josh]  # => []
 aliases         # => {:josh=>[]}
 aliases[:jeff] << 'the dude' << 'his dudeness' << 'duder' << 'el duderino'
-aliases         # => {:jeff=>["the dude", "his dudeness", "duder", "el duderino"], :josh=>[]}
-
-# you can alternatively set the default return value, and it will be returned if there is no key (instead of nil).
-# Be careful, though, it is the same object every time, so if you mutate it, it won't do what you expect
-# you don't really use this often.
-sums = Hash.new 5
-hash.default          # => nil
-sums.default          # => 5
-
-sums                  # => {}
-sums[:evens]          # => 5
-sums                  # => {}
-sums[:evens] += 1
-sums                  # => {:evens=>6}
-
+aliases         # => {:josh=>[], :jeff=>["the dude", "his dudeness", "duder", "el duderino"]}
 
 
 # =====  Access and Assign  =====
@@ -49,7 +35,7 @@ hash                          # => {"my key"=>"my value"}
 hash["my key"]                # => "my value"
 
 # access multiple values at once
-hash = Hash[ *[ Array('a'..'j') , Array(1..10) ].transpose.flatten ]
+hash = Hash[ *[ Array('a'..'j') , Array(1..10) ].transpose.flatten ] # just populating
 hash                                  # => {"a"=>1, "b"=>2, "c"=>3, "d"=>4, "e"=>5, "f"=>6, "g"=>7, "h"=>8, "i"=>9, "j"=>10}
 hash.values_at 'j' , 'e' , 'i' , :x   # => [10, 5, 9, nil]
 
@@ -59,9 +45,9 @@ hash.fetch "a"                              # => 100
 hash.fetch "z" , "go fish"                  # => "go fish"
 hash.fetch("z") { |el| "go fish, #{el}"}    # => "go fish, z"
 
-# sometimes you might use has_key to check for a key, instead of dealing with default values
-hash = { :a => :b }
-hash.default = true
+# sometimes you might use has_key to check for a key, for example, if you're using an init block
+hash = Hash.new { true }
+hash[:a] = :b
 hash[1000]                                  # => true
 hash.has_key? 1000                          # => false
 
@@ -80,20 +66,20 @@ hash.has_value?(999)                        # => false
 # =====  Removal  =====
 # delete accepts a key and returns its value. You can give a block that will be invoked and returned
 # in the event that the keyt o delete was not seen
-hash = Hash[*Array(1..10)]                  # => {5=>6, 1=>2, 7=>8, 3=>4, 9=>10}
+hash = Hash[*Array(1..10)]                  # => {1=>2, 3=>4, 5=>6, 7=>8, 9=>10}
 hash.delete 1                               # => 2
 hash.delete 100                             # => nil
-hash                                        # => {5=>6, 7=>8, 3=>4, 9=>10}
+hash                                        # => {3=>4, 5=>6, 7=>8, 9=>10}
 hash.delete(3)   { |n| "#{n} not found" }   # => 4
 hash.delete(100) { |n| "#{n} not found" }   # => "100 not found"
 
 # delete_if to remove all pairs from a hash 
 # that cause the block to evaluate to true
-hash = Hash[*Array(1..10)]                  # => {5=>6, 1=>2, 7=>8, 3=>4, 9=>10}
+hash = Hash[*Array(1..10)]                  # => {1=>2, 3=>4, 5=>6, 7=>8, 9=>10}
 hash.delete_if do |key,value|
   key % 5 == 0 || value % 5 == 0
 end
-hash                                        # => {1=>2, 7=>8, 3=>4}
+hash                                        # => {1=>2, 3=>4, 7=>8}
 
 
 # =====  Iterating  =====
@@ -103,7 +89,7 @@ results = Array.new
 cities.each do |city,country|
   results << "#{city} is in #{country}"
 end
-results # => ["Paris is in France", "Ramstein is in Germany", "Chicago is in USA"]
+results # => ["Chicago is in USA", "Paris is in France", "Ramstein is in Germany"]
 
 
 # =====  Other Useful Methods  =====
@@ -117,8 +103,8 @@ custom                        # => {:city=>"Boston"}
 merged                        # => {:color=>:red, :city=>"Boston"}
 
 cities = { :Chicago => :USA , :Paris => :France , :Ramstein => :Germany }
-cities.keys                   # => [:Paris, :Ramstein, :Chicago]
-cities.values                 # => [:France, :Germany, :USA]
+cities.keys                   # => [:Chicago, :Paris, :Ramstein]
+cities.values                 # => [:USA, :France, :Germany]
 cities.length                 # => 3
 cities.size                   # => 3
 cities.empty?                 # => false
@@ -126,19 +112,19 @@ Hash.new.empty?               # => true
 
 
 hash = Hash[*Array(1..10)]
-hash                          # => {5=>6, 1=>2, 7=>8, 3=>4, 9=>10}
+hash                          # => {1=>2, 3=>4, 5=>6, 7=>8, 9=>10}
 hash.clear
 hash                          # => {}
 
 # get a key for a given value (remember, there could be more than one key with the same value)
 hash = { "a" => 100, "b" => 200 }
-hash.index(200)               # => "b"
-hash.index(999)               # => nil
+hash.index(200)               # => "b" # !> Hash#index is deprecated; use Hash#key
+hash.index(999)               # => nil # !> Hash#index is deprecated; use Hash#key
 
 # swap keys with values values
 hash = { :n => 1, :m => 1, :o => 2 }  # n and m have the same value, m ends up getting squashed
-hash.invert                   # => {1=>:n, 2=>:o}
+hash.invert                   # => {1=>:m, 2=>:o}
 
 # convert to an array
-{ :Chicago => :USA , :Paris => :France , :Ramstein => :Germany }.to_a # => [[:Paris, :France], [:Ramstein, :Germany], [:Chicago, :USA]]
+{ :Chicago => :USA , :Paris => :France , :Ramstein => :Germany }.to_a # => [[:Chicago, :USA], [:Paris, :France], [:Ramstein, :Germany]]
 
