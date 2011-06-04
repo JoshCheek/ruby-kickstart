8b650f3ca98b77f23ca5a43b7263dbc6715ced0f
Josh Cheek <josh.cheek@gmail.com>
2011-06-04 00:26:57 -0500

    use only Proc.new, ignore the existence of lambda

--- ch3/solved/7.rb
+++ ch3/solved/7.rb
@@ -1,7 +1,7 @@
 def your_sort( array , &orderer )
 
   # if it is nil, then it hasn't been set, default to spaceship operator for comparison result
-  orderer = lambda { |a,b| a <=> b } unless orderer
+  orderer = Proc.new { |a,b| a <=> b } unless orderer
   
   array.each_index do |index1|
     array.each_index do |index2|
