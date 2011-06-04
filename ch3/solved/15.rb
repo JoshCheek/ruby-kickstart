bceaa47a99272fda82635e793024bd34d52ca755
Josh Cheek <josh.cheek@gmail.com>
2011-06-04 06:52:29 -0500

    clean up solution a bit

--- ch3/solved/15.rb
+++ ch3/solved/15.rb
@@ -1,13 +1,11 @@
 def list_size(list)
-  size = 0
+  size = 1
   size += 1 while list = list[:next]
-  size + 1
+  size
 end
-
 
 def middle(list)
   middle = list_size(list) / 2
   middle.times { list = list[:next] }
   list[:data]
 end
-
