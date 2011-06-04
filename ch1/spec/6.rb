b9852d115c7b690d1cc8b59bad609878d390df90
Josh Cheek <josh.cheek@gmail.com>
2011-06-04 07:27:49 -0500

    remove unicode chars since they completely die on 1.8.7

--- ch1/spec/6.rb
+++ ch1/spec/6.rb
@@ -1,5 +1,3 @@
-# encoding: utf-8
-
 describe 'odds_and_evens' do
   
   context 'when odd' do
@@ -15,9 +13,9 @@ describe 'odds_and_evens' do
     it 'should return "123456789" when given "112233445566778899"' do
       odds_and_evens("112233445566778899",true).should == "123456789"
     end
-    it 'should work for "“You want to enjoy life, don\'t you? If you get your job done quickly and your job is fun, that\'s good isn\'t it? That\'s the purpose of life, partly. Your life is better.” -- Yukihiro Matsumoto"' do
-      odds_and_evens("“You want to enjoy life, don\'t you? If you get your job done quickly and your job is fun, that\'s good isn\'t it? That\'s the purpose of life, partly. Your life is better.” -- Yukihiro Matsumoto",true).should ==
-        "Yuwn oejylf,dntyu fyugtyu o oeqikyadyu o sfn htsgo s' t htsteproeo ie aty orlf sbte. -Ykhr asmt"
+    it 'should work for "You want to enjoy life, don\'t you? If you get your job done quickly and your job is fun, that\'s good isn\'t it? That\'s the purpose of life, partly. Your life is better. -- Yukihiro Matsumoto"' do
+      odds_and_evens("You want to enjoy life, don\'t you? If you get your job done quickly and your job is fun, that\'s good isn\'t it? That\'s the purpose of life, partly. Your life is better. -- Yukihiro Matsumoto",true).should ==
+        "o att no ie o' o?I o e orjbdn ucl n orjbi u,ta' odinti?Ta' h ups flf,prl.Yu iei etr -Ykhr asmt"
     end
   end
   
@@ -34,9 +32,9 @@ describe 'odds_and_evens' do
     it 'should return "123456789" when given "112233445566778899"' do
       odds_and_evens("112233445566778899",false).should == "123456789"
     end
-    it 'should work for "“You want to enjoy life, don\'t you? If you get your job done quickly and your job is fun, that\'s good isn\'t it? That\'s the purpose of life, partly. Your life is better.” -- Yukihiro Matsumoto"' do
-      odds_and_evens("“You want to enjoy life, don\'t you? If you get your job done quickly and your job is fun, that\'s good isn\'t it? That\'s the purpose of life, partly. Your life is better.” -- Yukihiro Matsumoto",false).should ==
-        "“o att no ie o' o?I o e orjbdn ucl n orjbi u,ta' odinti?Ta' h ups flf,prl.Yu iei etr”- uiioMtuoo"
+    it 'should work for "You want to enjoy life, don\'t you? If you get your job done quickly and your job is fun, that\'s good isn\'t it? That\'s the purpose of life, partly. Your life is better. -- Yukihiro Matsumoto"' do
+      odds_and_evens("You want to enjoy life, don\'t you? If you get your job done quickly and your job is fun, that\'s good isn\'t it? That\'s the purpose of life, partly. Your life is better. -- Yukihiro Matsumoto",false).should ==
+        "Yuwn oejylf,dntyu fyugtyu o oeqikyadyu o sfn htsgo s' t htsteproeo ie aty orlf sbte.- uiioMtuoo"
     end
     
   end
