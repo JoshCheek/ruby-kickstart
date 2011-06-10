08a0014c4826eace70b82b8caa75e3b0a535445e
Josh Cheek <josh.cheek@gmail.com>
2011-06-10 03:40:54 -0500

    fix typo

--- ch2/notes/13-initializing.rb
+++ ch2/notes/13-initializing.rb
@@ -1,13 +1,13 @@
 # When you instantiate an object (make an instance 
 # of a class), you use the new method. After doing all
-# the things necessary to make an object, th new method 
+# the things necessary to make an object, thenew method 
 # will invoke a method named initialize, and pass it 
 # any arguments you passed to new. This allows you to 
 # put your object into an initial state.
 
 class Instrument
   
-  def initialize(name, price)
+  def initialize(name, price)
     @name = name
     @price = price
   end
