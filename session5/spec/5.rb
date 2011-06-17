209669161eee226249e287e5c46fd6d75a9f45f6
Josh Cheek <josh.cheek@gmail.com>
2011-06-17 16:41:36 -0500

    make the tests for this suck less

--- session5/spec/5.rb
+++ session5/spec/5.rb
@@ -1,22 +1,24 @@
-THELIST = Module.constants.grep(/error|exception/i)
-
-def should_find(error_to_find)
-  Object.const_set error_to_find , Class.new(StandardError)
-  THELIST << Module.constants.grep(/^#{error_to_find}$/).first
-  it "should find #{error_to_find}" do
-    list_of_errors_and_exceptions.sort.should == THELIST.sort
-  end
-end
-
+errors = Module.constants.grep(/error|exception/i)
 
 describe 'list_of_errors_and_exceptions' do
   
+  # This sort of absurdness is to ensure we are explicitly looking
+  # for the error, rather than replicating the same code in the
+  # tests as the solution and having tests pass when they should fail.
+  should_find = Proc.new do |error_to_find|
+    Object.const_set error_to_find , Class.new(StandardError)
+    errors << (errors.first.is_a?(Symbol) && error_to_find.to_sym || error_to_find.to_s) # this changes between 1.8 and 1.9, just make it work for both
+    it "should find #{error_to_find}" do
+      list_of_errors_and_exceptions.sort.should == errors.sort
+    end
+  end
+  
   it 'should return an Array' do
-    list_of_errors_and_exceptions.is_a?(Array).should be_true
+    list_of_errors_and_exceptions.is_a?(Array).should be
   end
   
   it 'should return a list of all errors and exceptions' do
-    list_of_errors_and_exceptions.sort.should == THELIST.sort
+    list_of_errors_and_exceptions.sort.should == errors.sort
   end
   
   %w(
@@ -28,7 +30,8 @@ describe 'list_of_errors_and_exceptions' do
     ExcEPtion 
     Abcexceptiondef
   ).each do |error_to_find|
-    should_find error_to_find
+    should_find.call error_to_find
   end
   
 end
+
