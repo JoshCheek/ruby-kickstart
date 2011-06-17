96da02cb2dad01ac9da88b6170c82e63690c8bdc
Josh Cheek <josh.cheek@gmail.com>
2011-06-17 13:19:33 -0500

    add conclusion

---
+++ session5/notes/19-go-do-the-challenges.rb
@@ -0,0 +1,10 @@
+# !!!LETS TAKE A VOTE!!!
+# Do you guys want to have one more session where we make some sort
+# of simple web app with Sinatra and put it online?
+
+
+# Okay go try out the challenges now. Don't forget, there's
+# cheatsheets for regular expressions
+#   https://github.com/JoshCheek/ruby-kickstart/blob/master/cheatsheets/regular_expressions.rb
+# and for rubygems
+#   https://github.com/JoshCheek/ruby-kickstart/blob/master/cheatsheets/rubygems.rb
