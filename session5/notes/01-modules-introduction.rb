b909fc0953c4f65acaa0348428d5820c7bb39252
Josh Cheek <josh.cheek@gmail.com>
2011-06-19 03:11:03 -0500

    fix mistake in text

--- session5/notes/01-modules-introduction.rb
+++ session5/notes/01-modules-introduction.rb
@@ -6,7 +6,7 @@
 # A class can only inherit from one other class, but it can include
 # as many modules as it likes (this is commonly called a mix-in)
 
-# Because of this (among other things), subclassing is usually
+# Because of this (among other things), mixing modules in is usually
 # preferred in the Ruby community, rather than subclassing.
 # Now, people usually prefer to get functionality by mixing in a
 # module rather than subclassing another class.
