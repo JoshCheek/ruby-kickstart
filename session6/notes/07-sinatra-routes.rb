79285442bf42d8faf62548f8af4a3f7924f57938
Josh Cheek <josh.cheek@gmail.com>
2011-06-29 16:32:00 -0500

    needs sinatra to run

--- session6/notes/07-sinatra-routes.rb
+++ session6/notes/07-sinatra-routes.rb
@@ -2,6 +2,7 @@
 # Sinatra will use regular expressions to find the variable and put it
 # in the params hash under the name you have specified
 
+require 'sinatra'
 get '/hello/:first_name' do
   "Hello, #{params[:first_name]}, glad you could join us."
 end
