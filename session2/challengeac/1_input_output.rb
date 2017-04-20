00a550c169fcf53c312536d63e58d188161afc56
Andrew Clarke <andrewclarke982@gmail.com>
2017-04-20 13:30:09 +0400

    challenge1

---
+++ session2/challengeac/1_input_output.rb
@@ -0,0 +1,15 @@
+# Remember you can test this code with
+#   $ rake 2:1
+
+# Write a program that reads in two integers typed on the keybaord
+# and outputs their sum, difference, and product
+#
+# Standard input will be like "9 2\n" and will expect you to print
+# "11\n7\n18\n" to standard output.
+
+def sum_difference_product
+  a , b = gets.split.map { |x| x.to_i }
+  puts a + b
+  puts a - b
+  puts a * b
+end
