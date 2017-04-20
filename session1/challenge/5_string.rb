b5db12e52357a57308ada86097d029fb8e7e7411
Andrew Clarke <andrewclarke982@gmail.com>
2017-04-20 12:38:24 +0400

    Session 1 challenge

--- session1/challenge/5_string.rb
+++ session1/challenge/5_string.rb
@@ -1,8 +1,14 @@
 # Given a string, replace every instance of sad to happy
-# 
+#
 # add_more_ruby("The clowns were sad.")         # => "The clowns were happy."
 # add_more_ruby("The sad dad said sad stuff.")  # => "The happy dad said happy stuff."
 # add_more_ruby("Sad times are ahead!")         # => "Happy times are ahead!"
 
 def add_more_ruby(string)
+  string.gsub("sad", "happy")
+  string.gsub("Sad", "Happy")
 end
+
+puts add_more_ruby("The clowns were sad.")
+puts add_more_ruby("The sad dad said sad stuff.")
+puts add_more_ruby("Sad times are ahead!")
--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -1,11 +1,23 @@
 # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
-# 
+#
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-# 
+#
 # odds_and_evens("abcdefg",true)    # => "bdf"
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
-def odds_and_evens(string, return_odds)
-end
+  def odds_and_evens(string, return_odds)
+    to_return = ""
+    string.size.times do |index|
+      next if return_odds && index.even?
+      next if !return_odds && index.odd?
+      to_return << string[index]
+    end
+    to_return
+  end
+
+  puts odds_and_evens("abcdefg",true)
+  puts odds_and_evens("abcdefg",false)
+
+  # ok so needed help with this one
