d07cc6b157fcb2af05125e2c022e9c46115d3dc7
Alex <alex@scotttonge.com>
2017-06-07 12:31:48 +0100

    2:11

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -12,9 +12,50 @@
 # Zero bottles of beer on the wall.
 #
 # Your program should not use ninety-nine output statements!
-# Design your program with a class named BeerSong whose initialize method 
+# Design your program with a class named BeerSong whose initialize method
 # receives a parameter indicating the number of bottles of beer initially on the wall.
 # If the parameter is less than zero, set the number of bottles to zero. Similarly,
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+class BeerSong
+attr_accessor :beers
+
+def initialize(beers)
+  beers = 0 if beers < 0
+  beers = 99 if beers > 99
+  self.beers = beers
+end
+
+def print_song
+  beers.downto 1 do |i|
+    print_stanza i
+  end
+end
+
+def print_stanza(n)
+  if n.zero?
+    String.new
+  else
+    puts "#{translate n} #{bottle n} of beer on the wall,"
+    "#{translate n} #{bottle n} of beer,"
+    "Take on down, pass it around,"
+    "#{translate n - 1} #{bottle n - 1} of beer on the wall."
+  end
+end
+
+def bottle(n)
+  if n == 1 then 'bottle' else 'bottles' end
+  end
+
+  def translate(n)
+    if 0 <= n && n <= 19
+      %w(zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fiftenn sixteen seventeen eighteen nineteen)
+    elseif n % 10 == 0
+    %w(zero ten twenty thirty forty fifty sixty seventy eigthy ninety)[n/10]
+  else
+    "#{translate n/10*10}-#{translate n%10}".downcase
+  end.capitalize
+end
+end
