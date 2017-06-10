82f245a0563be76d4aa4d30d9ee0a912fe728a80
Phil Lang <plangphoto@gmail.com>
2017-06-10 11:00:21 +0100

    completed challenge 5

--- session2/challenge/5_array.rb
+++ session2/challenge/5_array.rb
@@ -1,7 +1,7 @@
-# Write a function named mod_three which takes an array of numbers, 
+# Write a function named mod_three which takes an array of numbers,
 # and return a new array consisting of their remainder when divided by three.
 # Exclude any numbers which are actually dividible by three.
-# 
+#
 # EXAMPLES:
 # mod_three [0]  # => []
 # mod_three [1]  # => [1]
@@ -13,3 +13,6 @@
 # mod_three [7]  # => [1]
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
+def mod_three(array)
+ array.select { |num| num % 3 !=0 }.map { |num| num% 3 }
+end
