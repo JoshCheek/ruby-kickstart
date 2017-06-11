352e0aabda886de2d8e66644e365ec2d01ea73fb
Phil Lang <plangphoto@gmail.com>
2017-06-11 10:57:09 +0100

    completed challenge 7, challenge 6 uncompleted

--- session2/challenge/6_array.rb
+++ session2/challenge/6_array.rb
@@ -1,16 +1,30 @@
 # Write a method named prime_chars? which takes array of strings
-# and returns true if the sum of the characters is prime. 
-# 
-# Remember that a number is prime if the only integers that can divide it with no remainder are 1 and itself. 
-# 
+# and returns true if the sum of the characters is prime.
+#
+# Remember that a number is prime if the only integers that can divide it with no remainder are 1 and itself.
+#
 # Examples of length three
 # prime_chars? ['abc']            # => true
 # prime_chars? ['a', 'bc']        # => true
 # prime_chars? ['ab', 'c']        # => true
 # prime_chars? ['a', 'b', 'c']    # => true
-# 
+#
 # Examples of length four
 # prime_chars? ['abcd']           # => false
 # prime_chars? ['ab', 'cd']       # => false
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
+
+def string_length(array)
+  array.join.length
+end
+
+def prime_chars?(num)
+  return false if num < 2
+  2.upto Math.sqrt(num) do |i|
+    return false if num % i == 0
+  end
+  true
+end
+
+prime_chars? string_length ['abc']
--- session2/challenge/7_array.rb
+++ session2/challenge/7_array.rb
@@ -3,8 +3,27 @@
 # In order to not have to write an actual language parser, there won't be any punctuation too complex.
 # There will be no "'" that is not part of a contraction.
 # Assume each of these  charactsrs are not to be considered: ! @ $ # % ^ & * ( ) - = _ + [ ] : ; , . / < > ? \ |
-# 
+#
 # Examples
 # alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+
+def alternate_words(strings)
+  # Remove punctuation
+  '!@$#%^&*()-=_+[]:;,./<>?\\|'.split(//).each do |char|
+    strings = strings.gsub(char, ' ')
+  end
+  p strings
+  # Split the returning sentence up into an array
+  word = strings.split
+  p word
+  # Create an empty array
+  array = []
+  # Loop through each object in the array and only push even ones back into the array
+  word.each_with_index { |x, y| array << x if y.even? }
+  # Return array
+  p array
+  array
+end
+alternate_words("Can't we all get along?")
