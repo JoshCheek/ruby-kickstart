a75c8d4e2f94c537a55439a4f05e03fdcc6bbe0f
Phil Lang <plangphoto@gmail.com>
2017-06-12 14:44:20 +0100

    completed challenge 3:4

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,12 @@
 # There will be no punctuation in the strings.
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
+
+def first_pos(str)
+  # Empty hash
+  hash = {}
+  # Split the string, loop through each with index
+  # Append one instance of each word with the index at which it is found
+  str.split.each_with_index { |word, index| hash[word] ||= index }
+  hash
+end
