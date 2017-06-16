794ec5ba7d30bc6a6ee56af7ba045c91246d64f1
Phil Lang <plangphoto@gmail.com>
2017-06-16 22:54:17 +0100

    completed challenge 13

--- session3/challenge/13_proc.rb
+++ session3/challenge/13_proc.rb
@@ -52,5 +52,14 @@
 #   end
 # end       # => ["a", "m", "r", 1, 3, 4, 9, 2.5, 9.0, 25.8]
 
-def your_sort
+def your_sort(array, &block)
+  block ||= Proc.new { |a, b| a <=> b }
+  array.each_index do |ind1|
+    array.each_index do |ind2|
+      order = block.call(array[ind1], array[ind2])
+      p order
+      array[ind1], array[ind2] = array[ind2], array[ind1] if order < 0
+    end
+  end
 end
+your_sort [24, 0, 68, 44, 68, 47, 42, 66, 89, 22]  # => [0, 22, 24, 42, 44, 47, 66, 68, 68, 89]
